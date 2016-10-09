d68abd8eb40f400f8afccd26810819ff959e6ac0
Joonho Park <joonho.p@gmail.com>
2016-10-09 10:29:34 -0400

    read

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import csv
 def main():
 
 	with open('transaction_data_8451.csv', 'rb') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 		for row in spamreader:
-			if row[0] == "232" :
-				print (', '.join(row))
+			print row
+			print len(row)
 	
 main()
